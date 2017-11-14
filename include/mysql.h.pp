4070107d42093c26f9dc8ed43ad2db6f908da98a
Manuel Ung <mung@fb.com>
2017-11-13 22:24:32 -0800

    Add client error codes to distinguish between network timeout and loss
    Summary:
    Previously, mysql clients returned CR_SERVER_LOST on any network exception,
    including timeout.
    
    This adds two new client error codes `CR_NET_READ_INTERRUPTED` and
    `CR_NET_WRITE_INTERRUPTED` which is returned on network timeout.
    `CR_SERVER_LOST` is still returned for everything else.
    
    Reference patches:
    https://github.com/facebook/mysql-5.6/commit/e3052e1
    https://github.com/facebook/mysql-5.6/commit/e5b99cd
    
    Differential Revision: D6325225 (https://github.com/facebook/mysql-5.6/commit/323786b849944b9e7a2d9a59d47eb0ae0450106e)
    
    fbshipit-source-id: b7f356ffc9c

--- include/mysql.h.pp
+++ include/mysql.h.pp
@@ -359,7 +359,7 @@ void init_client_errs(void);
 void finish_client_errs(void);
 extern const char *client_errors[];
 static inline const char *ER_CLIENT(int client_errno) {
-  if (client_errno >= 2000 && client_errno <= 2070)
+  if (client_errno >= 2000 && client_errno <= 2072)
     return client_errors[client_errno - 2000];
   return client_errors[2000 - 2000];
 }
--- libmysql/errmsg.cc
+++ libmysql/errmsg.cc
@@ -115,6 +115,8 @@ const char *client_errors[] = {
     "access.",
     "Determining the real path for '%s' failed with error (%d): %s",
     "DNS SRV lookup failed with error : %d",
+    "Read timeout is reached",
+    "Write timeout is reached",
     ""};
 
 static const char *get_client_errmsg(int nr) {
