f6c9fce0b319d250ee1ac061ff99e8033a3d622a
Manuel Ung <mung@fb.com>
2020-01-22 14:04:31 -0800

    Add CPU time to statement metrics
    Summary: Add cpu time to performance schema metrics.
    
    Differential Revision: D19539990 (https://github.com/facebook/mysql-5.6/commit/b9b78a98802fb3286c1fa6e97f7fd48f0bcdb213)
    
    fbshipit-source-id: e9e79d21159

--- storage/perfschema/pfs.cc
+++ storage/perfschema/pfs.cc
@@ -5926,6 +5926,7 @@ PSI_statement_locker *pfs_get_thread_statement_locker_v2(
       pfs->m_timer_start = 0;
       pfs->m_timer_end = 0;
       pfs->m_lock_time = 0;
+      pfs->m_cpu_time = 0;
       pfs->m_current_schema_name_length = 0;
       pfs->m_sqltext_length = 0;
       pfs->m_sqltext_truncated = false;
@@ -6036,6 +6037,7 @@ PSI_statement_locker *pfs_get_thread_statement_locker_v2(
   state->m_flags = flags;
 
   state->m_lock_time = 0;
+  state->m_cpu_time = 0;
   state->m_rows_sent = 0;
   state->m_rows_examined = 0;
   state->m_rows_deleted = 0;
@@ -6252,6 +6254,11 @@ void pfs_set_statement_query_id_v2(PSI_statement_locker *locker,
 void pfs_set_statement_lock_time_v2(PSI_statement_locker *locker,
                                     ulonglong count) {
   SET_STATEMENT_ATTR_BODY(locker, m_lock_time, count);
+}
+
+void pfs_set_statement_cpu_time_v2(PSI_statement_locker *locker,
+                                   ulonglong count) {
+  SET_STATEMENT_ATTR_BODY(locker, m_cpu_time, count);
 }
 
 void pfs_set_statement_rows_sent_v2(PSI_statement_locker *locker,
@@ -6478,6 +6485,7 @@ void pfs_end_statement_v2(PSI_statement_locker *locker, void *stmt_da) {
   }
 
   stat->m_lock_time += state->m_lock_time;
+  stat->m_cpu_time += state->m_cpu_time;
   stat->m_rows_sent += state->m_rows_sent;
   stat->m_rows_examined += state->m_rows_examined;
   stat->m_rows_deleted += state->m_rows_deleted;
@@ -6559,6 +6567,7 @@ void pfs_end_statement_v2(PSI_statement_locker *locker, void *stmt_da) {
     }
 
     digest_stat->m_stat.m_lock_time += state->m_lock_time;
+    digest_stat->m_stat.m_cpu_time += state->m_cpu_time;
     digest_stat->m_stat.m_rows_sent += state->m_rows_sent;
     digest_stat->m_stat.m_rows_examined += state->m_rows_examined;
     digest_stat->m_stat.m_rows_deleted += state->m_rows_deleted;
@@ -6600,6 +6609,7 @@ void pfs_end_statement_v2(PSI_statement_locker *locker, void *stmt_da) {
       }
 
       sub_stmt_stat->m_lock_time += state->m_lock_time;
+      sub_stmt_stat->m_cpu_time += state->m_cpu_time;
       sub_stmt_stat->m_rows_sent += state->m_rows_sent;
       sub_stmt_stat->m_rows_examined += state->m_rows_examined;
       sub_stmt_stat->m_rows_deleted += state->m_rows_deleted;
@@ -6645,6 +6655,7 @@ void pfs_end_statement_v2(PSI_statement_locker *locker, void *stmt_da) {
         }
 
         prepared_stmt_stat->m_lock_time += state->m_lock_time;
+        prepared_stmt_stat->m_cpu_time += state->m_cpu_time;
         prepared_stmt_stat->m_rows_sent += state->m_rows_sent;
         prepared_stmt_stat->m_rows_examined += state->m_rows_examined;
         prepared_stmt_stat->m_rows_deleted += state->m_rows_deleted;
@@ -8401,6 +8412,7 @@ PSI_statement_service_v2 pfs_statement_service_v2 = {
     pfs_set_statement_text_v2,
     pfs_set_statement_query_id_v2,
     pfs_set_statement_lock_time_v2,
+    pfs_set_statement_cpu_time_v2,
     pfs_set_statement_rows_sent_v2,
     pfs_set_statement_rows_examined_v2,
     pfs_inc_statement_rows_deleted_v2,
@@ -8442,6 +8454,7 @@ SERVICE_IMPLEMENTATION(performance_schema, psi_statement_v1) = {
     pfs_start_statement_v2,
     pfs_set_statement_text_v2,
     pfs_set_statement_lock_time_v2,
+    pfs_set_statement_cpu_time_v2,
     pfs_set_statement_rows_sent_v2,
     pfs_set_statement_rows_examined_v2,
     pfs_inc_statement_rows_deleted_v2,
@@ -8484,6 +8497,7 @@ SERVICE_IMPLEMENTATION(performance_schema, psi_statement_v2) = {
     pfs_set_statement_text_v2,
     pfs_set_statement_query_id_v2,
     pfs_set_statement_lock_time_v2,
+    pfs_set_statement_cpu_time_v2,
     pfs_set_statement_rows_sent_v2,
     pfs_set_statement_rows_examined_v2,
     pfs_inc_statement_rows_deleted_v2,
